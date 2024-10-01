cafe1ca40fe9b7eeb5a01ee61976c42b30550efb
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-10-01 18:19:30 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const Cronometro = () => {
+
+  // estado do cronômetro (true é rodando, false é não rodando)
+  const [estado, setEstado] = useState(false)
+  const [tempo, setTempo] = useState(0) // tempo em milissegundos
+  const intervalRef = useRef(null);  // Intervalo do cronômetro
+
+
+  function inicializarCronometro () {
+    if (!estado) {
+      setEstado(true)
+      intervalRef.current = setInterval(() => {
+        setTempo(prevTime => prevTime + 10)
+      }, 10)
+    }
+  }
+
+  function pausarTimer () {
+    if (estado) {
+      clearInterval(intervalRef.current)
+      setEstado(false)
+    }
+  }
+
+  function resetarTimer () {
+    clearInterval(intervalRef.current);
+    setEstado(false);
+    setTempo(0);
+  }
+
+
+  function formatarTempo () {
+    const minutes = Math.floor(tempo / 60000);
+    const seconds = Math.floor((tempo % 60000) / 1000);
+    const milliseconds = Math.floor((tempo % 1000) / 10);
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')},${milliseconds.toString().padStart(2, '0')}`
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.timerText}>{formatarTempo()}</Text>
+      <Button title={estado ? 'Pausar' : 'Iniciar'} onPress={estado ? pausarTimer : inicializarCronometro} />
+      <Button title="Resetar" onPress={resetarTimer} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  timerText: {
+    fontSize: 72,
+    fontWeight: 'semibold',
+    marginBottom: 20,
   },
 });
+
+export default Cronometro;
